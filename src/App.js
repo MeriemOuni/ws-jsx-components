17138b3717730296cd74774cd9d0910ac80c70e3
MeriemOuni <elounimeriem@gmail.com>
2024-11-27 15:08:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import profile from "./profile.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1 style={{ color: "red", fontSize: "36px" }}>
+				{" "}
+				Workshop React JSX & Components
+			</h1>
+			<p></p>
+			<br />
+			<img src={profile} alt='profile-pic' />
+			<img
+				style={{ width: "40%" }}
+				src='https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png'
+				alt='profile-img'
+			/>
+		</div>
+	);
 }
 
 export default App;
